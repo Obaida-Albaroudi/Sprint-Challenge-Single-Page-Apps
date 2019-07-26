5f2643b55fc342f51a0601fdc6bc3f43b2b50346
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-07-26 08:31:31 -0700

    Completed the axios pull for all three datasets. Finished LocationsList working on the other two now.

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,21 @@
 import React, { useEffect, useState } from 'react';
 
-export default function LocationsList() {
+export default function LocationsList({Locations}) {
+    const location = data.map((information) =>
+    <Card key={information}>
+        <h1>{information.name}</h1>   
+        <div>
+            <Content>Type: {information.type}</Content>
+            <Content>Dimenstions: {information.dimensions}</Content>
+        </div>
+    </Card> 
+    )
+    //Over here I just return person as it represents each iteration of the array which is an object which is a person.
+    return(
+        <div >
+            {location}
+        </div>
+ 
+    )
 
 }
